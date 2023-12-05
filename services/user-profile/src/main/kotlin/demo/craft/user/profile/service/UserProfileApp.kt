648a90a73d827e25ea3cd47b56e9ea688c26edf2
Preetom Bhowmik <preetombhowmik@Preetoms-MacBook-Pro.local>
2023-12-05 23:50:48 +0530

    add annotation

--- services/user-profile/src/main/kotlin/demo/craft/user/profile/service/UserProfileApp.kt
+++ services/user-profile/src/main/kotlin/demo/craft/user/profile/service/UserProfileApp.kt
@@ -10,6 +10,7 @@ import java.util.TimeZone
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(
     scanBasePackages = [
@@ -27,6 +28,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
         CacheProperties::class,
     ]
 )
+@EnableJpaRepositories
 class UserProfileApp {
     init {
         TimeZone.setDefault(TIME_ZONE_UTC)
